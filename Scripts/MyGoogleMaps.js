33db6e5f439dda82dbbb96590703acc38969f9ba
ansiitis <ansisupenieks@gmail.com>
2016-10-26 20:46:36 +0300

    Add files via upload

---
+++ Scripts/MyGoogleMaps.js
@@ -0,0 +1,40 @@
+﻿var map;
+var infowindow;
+
+function initMap() {
+    var wtv = { lat: 56.952131, lng: 24.1238028 };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: wtv,
+        zoom: 15
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: wtv,
+        radius: 500,
+        type: [tags.value]
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
---
+++ Scripts/MyURLShortner.js
@@ -0,0 +1,13 @@
+﻿function GoogleShortenURLAPI() {
+    $.ajax({
+        url: "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyClb1gbn3hFP1-PNIcC9N9QGattF9xkZ2Q",
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        data: '{ longUrl:"' + longURL.value + '"}',
+        success: function (response) {
+            $('#ShortURL').text(response.id);
+            $("#ShortURL").attr("href", response.id);
+        },
+        error: function (msg) { alert(msg); }
+    });
+};
